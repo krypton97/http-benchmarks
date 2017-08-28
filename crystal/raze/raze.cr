83bf937ffe75fd57960a2fa87dde58e9259c2d80
alex <bldcuber@gmail.com>
2017-08-28 22:37:40 +0300

    raze config

--- crystal/raze/raze.cr
+++ crystal/raze/raze.cr
@@ -4,7 +4,6 @@ Raze.config.host = "127.0.0.1"
 Raze.config.env = "production"
 Raze.config.logging = false
 Raze.config.compress = false
-Raze.config.always_rescue = false
 Raze.config.static_indexing = false
 
 get "/" do
